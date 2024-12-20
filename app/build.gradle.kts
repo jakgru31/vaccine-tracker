14eb4fcf6360362d1c0f6ebc53f7fed6cf5aa523
Nazarii171717 <nazarzavgar@gmail.com>
2024-12-20 13:24:03 +0100

    19.12.2024

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,7 @@ dependencies {
     implementation(libs.firebase.auth.ktx)
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.activity)
+    implementation(libs.firebase.firestore.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
